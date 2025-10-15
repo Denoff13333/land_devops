08fea35cc3ac0027d81ad9798b8e5fb8d0aeb48c
Naumkin_Danila <Denilnm2005@gmail.com>
2025-10-15 14:05:58 +0300

    Update notify_telegram.sh

--- .github/scripts/notify_telegram.sh
+++ .github/scripts/notify_telegram.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
 BOT="$TELEGRAM_BOT_TOKEN"
 CHAT="$TELEGRAM_CHAT_ID"
 
@@ -9,12 +10,26 @@ PR="${3}"
 DOCKER_REPO="${4}"
 TAG="v${VERSION}"
 
-TEXT="*Новый выпуск изменений*\n\n*Проект:* ${PROJECT}\n*Версия:* ${VERSION}\n*GIT PR:* ${PR}\n*GIT TAG:* ${TAG}\n\n*Docker:* \`${DOCKER_REPO}:${TAG}\`"
+TEXT="*Новый выпуск изменений*\n\n\
+*Проект:* ${PROJECT}\n\
+*Версия:* ${VERSION}\n\
+*Дата:* $(date '+%Y.%m.%d %H:%M:%S')\n\
+*Автор:* $(git log -1 --pretty=format:'%an')\n\n\
+*Информация о Git-репозитории*\n\
+GIT MR: [${PR}](https://github.com/${GITHUB_REPOSITORY}/pull/${PR})\n\
+GIT TAG: [${TAG}](https://github.com/${GITHUB_REPOSITORY}/releases/tag/${TAG})\n\n\
+*Информация о Docker-репозитории*\n\
+Владелец: ${DOCKER_REPO%%/*}\n\
+Название: ${PROJECT}\n\
+Тег: ${TAG}\n\
+Полное имя: [${DOCKER_REPO}:${TAG}](https://hub.docker.com/r/${DOCKER_REPO})"
+
 
 curl -sS "https://api.telegram.org/bot${BOT}/sendMessage" \
   -d chat_id="${CHAT}" \
-  -d parse_mode=Markdown \
+  -d parse_mode=MarkdownV2 \
   --data-urlencode "text=${TEXT}" >/dev/null
+
 
 curl -sS -F chat_id="${CHAT}" -F document="@CHANGELOG.md" \
   "https://api.telegram.org/bot${BOT}/sendDocument" >/dev/null
